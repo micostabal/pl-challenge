1fa869df967dc9d166cf4bad0e0c21ed691ea6ec
Martín Costabal <13747700+micostabal@users.noreply.github.com>
2023-06-27 13:03:35 -0400

    cli tool for replit

--- main.rb
+++ main.rb
@@ -2,17 +2,29 @@ require_relative "client"
 require_relative "loader"
 require_relative "domain"
 
-artists_file = File.open "./artists.json"
+ARTISTS_FILE = "./artists.json"
+CREDENTIALS_FILE = "./credentials.json"
+
+artists_file = File.open ARTISTS_FILE
 
 artists_data = JSON.load artists_file
 
-credentials_file = File.open "./credentials.json"
-
-auth_data = JSON.load credentials_file
+if File.file?(CREDENTIALS_FILE)
+  credentials_file = File.open CREDENTIALS_FILE
+  auth_data = JSON.load credentials_file
+  
+  client_id = auth_data['client_id']
+  client_secret = auth_data['client_secret']
+else 
+  puts "Enter client_id"
+  client_id = gets.chomp
+  puts "Enter client_secret"
+  client_secret = gets.chomp
+end
 
 client = SpotifyClient.new(
-  auth_data['client_id'],
-  auth_data['client_secret']
+  client_id,
+  client_secret
 )
 
 client.authenticate()
